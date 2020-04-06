4ccc2778f7db83a0b9f17438f1ddfd6013abe58c
Chris Bowling <chris2fer2002@gmail.com>
2020-04-06 16:48:52 -0400

    added some CSS transitions

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -9,7 +9,13 @@ import CampsiteInfo from './CampsiteInfoComponent';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { actions } from 'react-redux-form';
-import { postComment, fetchCampsites, fetchComments, fetchPromotions } from '../redux/ActionCreators';
+import {
+  postComment,
+  fetchCampsites,
+  fetchComments,
+  fetchPromotions,
+} from '../redux/ActionCreators';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 const mapStateToProps = (state) => {
   return {
@@ -23,14 +29,13 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = {
   postComment: (campsiteId, rating, author, text) =>
     postComment(campsiteId, rating, author, text),
-  fetchCampsites: () => (fetchCampsites()),
-  resetFeedbackForm: () => (actions.reset('feedbackForm')),
-  fetchComments: () => (fetchComments()),
-  fetchPromotions: () => (fetchPromotions())
+  fetchCampsites: () => fetchCampsites(),
+  resetFeedbackForm: () => actions.reset('feedbackForm'),
+  fetchComments: () => fetchComments(),
+  fetchPromotions: () => fetchPromotions(),
 };
 
 class Main extends Component {
-
   componentDidMount() {
     this.props.fetchCampsites();
     this.props.fetchComments();
@@ -41,10 +46,18 @@ class Main extends Component {
     const HomePage = () => {
       return (
         <Home
-          campsite={this.props.campsites.campsites.filter((campsite) => campsite.featured)[0]}
+          campsite={
+            this.props.campsites.campsites.filter(
+              (campsite) => campsite.featured
+            )[0]
+          }
           campsitesLoading={this.props.campsites.isLoading}
           campsitesErrMess={this.props.campsites.errMess}
-          promotion={this.props.promotions.promotions.filter((promotion) => promotion.featured)[0]}
+          promotion={
+            this.props.promotions.promotions.filter(
+              (promotion) => promotion.featured
+            )[0]
+          }
           promotionLoading={this.props.promotions.isLoading}
           promotionErrMess={this.props.promotions.errMess}
           partner={this.props.partners.filter((partner) => partner.featured)[0]}
@@ -74,22 +87,36 @@ class Main extends Component {
     return (
       <div>
         <Header />
-        <Switch>
-          <Route path='/home' component={HomePage} />
-          <Route
-            exact
-            path='/directory'
-            render={() => <Directory campsites={this.props.campsites} />}
-          />
-          <Route path='/directory/:campsiteId' component={CampsiteWithId} />
-          <Route
-            exact
-            path='/aboutus'
-            render={() => <About partners={this.props.partners} />}
-          />
-          <Route exact path='/contactus' render={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} />} />
-          <Redirect to='/home' />
-        </Switch>
+        <TransitionGroup>
+          <CSSTransition
+            key={this.props.location.key}
+            classNames='page'
+            timeout={300}
+          >
+            <Switch>
+              <Route path='/home' component={HomePage} />
+              <Route
+                exact
+                path='/directory'
+                render={() => <Directory campsites={this.props.campsites} />}
+              />
+              <Route path='/directory/:campsiteId' component={CampsiteWithId} />
+              <Route
+                exact
+                path='/aboutus'
+                render={() => <About partners={this.props.partners} />}
+              />
+              <Route
+                exact
+                path='/contactus'
+                render={() => (
+                  <Contact resetFeedbackForm={this.props.resetFeedbackForm} />
+                )}
+              />
+              <Redirect to='/home' />
+            </Switch>
+          </CSSTransition>
+        </TransitionGroup>
         <Footer />
       </div>
     );
